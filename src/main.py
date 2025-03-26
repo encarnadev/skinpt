e23cea8b93855ae16824a9854d75e5710b9271cb
encarnadev <encarnapieencarnao@gmail.com>
2025-03-26 09:21:38 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
-import time
+import asyncio
 import threading
 from flask import Flask
-from utils.skinport import sio
+from utils.skinport import run_bot
 
 app = Flask(__name__)
 
@@ -12,23 +12,14 @@ def home():
 def run_flask():
     app.run(host='0.0.0.0', port=8080)
 
-def run_bot():
-    while True:
-        try:
-            sio.connect('wss://skinport.com')
-            sio.wait()
-        except Exception as e:
-            print(f"Connection error: {e}")
-            time.sleep(5)  # Wait 5 seconds before reconnecting
-
 def main():
-    # Start Flask server in a separate thread
+    # Iniciar Flask en un hilo separado
     flask_thread = threading.Thread(target=run_flask)
     flask_thread.daemon = True
     flask_thread.start()
     
-    # Run the bot in the main thread
-    run_bot()
+    # Ejecutar el bot asíncrono
+    asyncio.run(run_bot())
 
 if __name__ == "__main__":
     main() 
